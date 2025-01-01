05a8d663c7f42bc0206351c446b82a7c2344edd3
CobraGamingSj <shashikaanthjain.1983@gmail.com>
2025-01-01 19:51:21 +0530

    initial testing

---
+++ src/main/java/org/cobra/api/tooltrim/render/ToolTrimLayerRenderer.java
@@ -0,0 +1,15 @@
+//package org.cobra.api.tooltrim.render;
+//
+//import net.minecraft.client.item.ItemModelManager;
+//import net.minecraft.client.render.item.ItemRenderer;
+//import net.minecraft.util.Identifier;
+//
+//public class ToolTrimLayerRenderer extends ItemRenderer {
+//
+//    private static final Identifier trimId = Identifier.of();
+//
+//    public ToolTrimLayerRenderer(ItemModelManager itemModelManager) {
+//        super(itemModelManager);
+//    }
+//
+//}
